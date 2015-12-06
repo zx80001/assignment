eb974704f09e105c3836680677ce9a56d03a8888
abc <michel@dd.com>
2015-12-06 14:42:35 +0800

    123

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var MONGODBURL ='mongodb://michellefurby.cloudapp.net:27017/test';
+//var MONGODBURL ='mongodb://localhost/test';
 
 var restaurantSchema = require('./models/restaurant');
 
@@ -100,29 +101,38 @@ app.get('/:attrib1/:attrib_value1/:attrib2/:attrib_value2', function(req,res) {
 	getRequest(res,criteria);
 });*/
 
-app.get('/:coord/:attrib_value1/:attrib_value2', function(req,res) {
+app.get('/coord/:attrib_value1/:attrib_value2', function(req,res) {
 	console.log("find coord with two value")
 	var criteria = {};
-	//var criteria = "{coord:{$in:["+req.params.attrib_value1+","+req.params.value2+"]}}";
-	//criteria["address."+req.params.coord]= "{$in:["+req.params.attrib_value1+","+req.params.attrib_value2+"]}";
+
+	criteria["address.coord.0"]= req.params.attrib_value1;
+	criteria["address.coord.1"] = req.params.attrib_value2;
 	console.log(criteria);
-	criteria["address."+req.params.coord]= req.params.attrib_value1;
-	criteria["address."+req.params.coord] = req.params.attrib_value2;
 	getRequest(res,criteria);
 });
 
-/*app.get('/:score/:attrib/:value', function(req,res){
+
+
+app.get('/score/:attrib/:attrib_value', function(req,res){
 	console.log("find score");
-	var criteria = "{grades:{$elemMatch:{"+req.params.score+":{$"+req.params.attrib+":"+req.params.value+"}}}}";
-	//var criteria = {};
-	//criteria["grade.score"]="{$"+req.params.criteria+":"+req.params.score_value+"}";
-	//criteria["grades."+req.params.score]= req.params.attrib_value1;
+
+	var value = parseInt(req.params.attrib_value);
+	var criteria ={};
+
+        var c ={};
+	c["$"+req.params.attrib]=value;
+	var b={};
+	b["score"]=c;
+	var a={};
+	a["$elemMatch"]=b;
+	criteria["grades"]=a;
+
 	getRequest(res,criteria);
 	
 });
-*/
+
 app.post('/',function(req,res) {
-	//console.log("add request");
+
 	var restaurantSchema = require('./models/restaurant');
 	mongoose.connect('mongodb://localhost/test');
 	var db = mongoose.connection;
@@ -140,8 +150,6 @@ app.post('/',function(req,res) {
 		rObj.cuisine = req.body.cuisine;
 		rObj.grades=[];
 		var a = {};
-		//rObj.grades.push({"date"req.body.date});
-		//console.log("sdfd");
 		if(req.body.date!=null){
   		 a.date = req.body.date;
   		}
@@ -154,24 +162,6 @@ app.post('/',function(req,res) {
   		if(req.body.date!=null||req.body.grade!=null||req.body.score!=null){
   		 rObj.grades.push(a);
   		}
-		//a.date = req.body.date;
-		//a.grade = req.body.grade;
-		//a.score = parseInt(req.body.score);
-		//rObj.grades.push(a);
-		//console.log(req.body.date);
-		//console.log(req.body.grade);
-		//console.log(req.body.score);
-
-		//console.log(a);
-		//for (var i=0;i<req.body.date.length;i++){
-		
-
-		//for (var i = 0; i < req.route.keys; i++) {
-		//for (var i = 0; i < req.grades.route.keys; i++) {
-    		
-		
-		
-		//}
 		
 		
 		rObj.name = req.body.name;
@@ -179,14 +169,15 @@ app.post('/',function(req,res) {
 
 		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
 		var r = new Restaurant(rObj);
-		//console.log(r);
+		
 		r.save(function(err) {
        		if (err) {
 				res.status(500).json(err);
 				throw err
 			}
-       		//console.log('Restaurant created!')
+       		
        		db.close();
+
 			res.status(200).json({message: 'insert done', id: r._id});
     	});
     });
@@ -196,7 +187,18 @@ app.post('/',function(req,res) {
 app.delete('/:attrib/:attrib_value',function(req,res) {
 	//console.log('Incoming request: DELETE');
 	var criteria = {};
-	criteria[req.params.attrib] = req.params.attrib_value;
+
+	if(req.params.attrib=="street"||req.params.attrib=="zipcode"||req.params.attrib=="building"){
+		criteria["address."+req.params.attrib] = req.params.attrib_value;
+	}
+	
+	else if (req.params.attrib=="date"||req.params.attrib=="grade"||req.params.attrib=="score"){
+		criteria["grades."+req.params.attrib] = req.params.attrib_value;
+	}
+	else{
+		criteria[req.params.attrib] = req.params.attrib_value;
+	}
+
 	mongoose.connect(MONGODBURL);
 	var db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
@@ -206,14 +208,61 @@ app.delete('/:attrib/:attrib_value',function(req,res) {
        		if (err) {
 				res.status(500).json(err);
 				throw err
-			}
-       		console.log('Restaurant removed!')
-       		db.close();
-			res.status(200).json({message: 'delete done', id: req.params.id});
+			}	
+		
+		res.status(200).json({message: 'delete done '+ req.params.attrib + ":"+req.params.attrib_value});
+		
+		//console.log('Restaurant removed!');
+		db.close();	
     	});
     });
 });
 
+app.delete('/:attrib1/:attrib_value1/:attrib2/:attrib_value2',function(req,res) {
+	//console.log('Incoming request: DELETE');
+	var criteria = {};
+
+	if (req.params.attrib1=="coord"&&req.params.attrib2=="coord"){
+		criteria["address.coord.0"]= req.params.attrib_value1;
+		criteria["address.coord.1"] = req.params.attrib_value2;
+	}
+	
+	if (req.params.attrib1=="date"||req.params.attrib1=="grade"||req.params.attrib1=="score"){
+		criteria["grades."+req.params.attrib1] = req.params.attrib_value1;
+	}
+	else if(req.params.attrib1=="street"||req.params.attrib1=="zipcode"||req.params.attrib1=="building"){
+		criteria["address."+req.params.attrib1] = req.params.attrib_value1;
+	}
+	else {
+		criteria[req.params.attrib1] = req.params.attrib_value1;
+	}
+
+	if (req.params.attrib2=="date"||req.params.attrib2=="grade"||req.params.attrib2=="score"){
+		criteria["grades."+req.params.attrib2] = req.params.attrib_value2;
+	}
+	else if(req.params.attrib2=="street"||req.params.attrib2=="zipcode"||req.params.attrib2=="building"){
+		criteria["address."+req.params.attrib2] = req.params.attrib_value2;
+	}
+	else {
+		criteria[req.params.attrib2] = req.params.attrib_value2;
+	}
+	mongoose.connect(MONGODBURL);
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function (callback) {
+	var Restaurant = mongoose.model('Restaurant', restaurantSchema);
+		Restaurant.remove(criteria,function(err,results){
+       		if (err) {
+				res.status(500).json(err);
+				throw err
+			}	
+		
+		res.status(200).json({message: 'delete done '+ req.params.attrib1 + ":"+req.params.attrib_value1+","+ req.params.attrib2 + ":"+req.params.attrib_value2});
+		//console.log('Restaurant removed!');
+		db.close();	
+    	});
+    });
+});
 
 
 app.put('/updateother/:findatt/:find_value/:attrib', function(req,res) {
